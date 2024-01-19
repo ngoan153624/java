ea43524bc98526b2fabd11e90c5c31b688a1f4c4
ngoan153624 <rinro153624@gmail.com>
2024-01-19 23:32:59 +0700

    init project

---
+++ bai3.js
@@ -0,0 +1,12 @@
+// Ma trận
+const matrix = [
+    [1, 1, 1],
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ];
+  
+  // In ma trận
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i].join(' '));
+  }
